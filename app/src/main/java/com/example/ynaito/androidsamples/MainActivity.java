c1d4077712eb157084cdbb7595119d897347d992
yuta1723 <yuta.n1723@gmail.com>
2018-07-10 02:59:11 +0900

    5000MSごとにAlarmManagerからintentを発行する機能を追加

--- app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
+++ app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.ynaito.androidsamples;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int period = 5000;
+    private String RE_BOOT = "REBOOT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!getIntent().getAction().equals(RE_BOOT)) {
+            schedAlarm();
+        }
+    }
+
+    public void schedAlarm() {
+        AlarmManager am = (AlarmManager) this.getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setAction(RE_BOOT);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        am.set(AlarmManager.RTC, System.currentTimeMillis() + period, pIntent);
     }
 }
+
+//https://akira-watson.com/android/app-restart.html
