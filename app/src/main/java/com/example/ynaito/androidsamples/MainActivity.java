8c964030fa012989ca80c48a01292ae9945bb142
yuta naito <y.naito@suvt.co.jp>
2018-07-09 18:41:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ynaito.androidsamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
