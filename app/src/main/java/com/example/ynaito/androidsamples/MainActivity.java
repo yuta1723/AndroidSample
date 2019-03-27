6051317b561156bfa89a01fba12c04b25aeff4be
yuta naito <yuta.n1723@gmail.com>
2019-03-27 16:37:07 +0900

    端末情報(OS,Model,PRODUCT NAME)を表示する機能を追加
    リリースするのに何も機能がないのはアレなので、一旦追加

--- app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
+++ app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ynaito.androidsamples;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView osView = (TextView)findViewById(R.id.testview01);
+        osView.setText("OS VERSION = " + Build.VERSION.RELEASE);
+
+        TextView modelView = (TextView)findViewById(R.id.testview02);
+        modelView.setText("MODEL = " + Build.MODEL);
+
+        TextView productView = (TextView)findViewById(R.id.testview03);
+        productView.setText("PRODUCT = " + Build.PRODUCT);
     }
 }
