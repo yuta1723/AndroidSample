d5f7825c6a935b75a98e70d4637a33a74e227f01
yuta1723 <yuta.n1723@gmail.com>
2019-04-14 12:20:09 +0900

    タブのレイアウトを下付きと上付きを切り替えれるように変更

--- app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
+++ app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,34 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+
+        switchTabLayout(tabLayout,RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+        // タブの追加方法
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+    }
+
+    private void switchTabLayout(TabLayout tabLayout, int attribute) {
+        Log.d(TAG,"switchTabLayout");
+        if (tabLayout == null) {
+            return;
+        }
+        if ((attribute != RelativeLayout.ALIGN_PARENT_TOP) && (attribute != RelativeLayout.ALIGN_PARENT_BOTTOM)) {
+            return;
+        }
+
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.addRule(attribute);
+        tabLayout.setLayoutParams(lp);
     }
 }
 
 
+
+
 // 参考
+// https://developer.android.com/reference/android/support/design/widget/TabLayout
 // https://qiita.com/PiyoMoasa/items/a0282483831b5993da02
+// https://qiita.com/furu8ma/items/90482dceeea8cfb162af
