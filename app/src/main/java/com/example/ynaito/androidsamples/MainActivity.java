34e8cd01ff7c78facb413ecfa6bb8849335b3935
yuta1723 <yuta.n1723@gmail.com>
2019-04-14 11:28:28 +0900

    MainActivity にタブを表示する機能を追加

--- app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
+++ app/src/main/java/com/example/ynaito/androidsamples/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ynaito.androidsamples;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabFragmentPagerAdapter adapter = new TabFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
